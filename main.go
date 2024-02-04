1acfeef03b002878b87fb9fd03b74198f472d3b6
Grace Quinn <gquinn7@outlook.com>
2024-02-04 15:53:23 +0000

    init concept

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"os"
 )
 
-func helloWorld() {
-	fmt.Println("Hello Go")
+func init() {
+	// sets up global context & is executed before main()
+	fmt.Print("Setting up Global Context")
+}
+
+func main() {
+	fmt.Println("\nHello Go")
 	fmt.Println("Current user:", os.Getenv("USER"))
 }
