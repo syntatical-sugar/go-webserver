cff22ab7564904140b955789b6f6fedcf39c2989
Grace Quinn <gquinn7@outlook.com>
2024-02-05 13:21:42 +0000

    Add a path handler

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net/http"
+	"time"
 )
 
 func init() {
 	// sets up global context & is executed before main()
-	fmt.Print("Setting up Global Context")
+	fmt.Println("Setting up Global Context")
 }
 
 func main() {
-	fmt.Println("\nHello Go")
-	fmt.Println("Current user:", os.Getenv("USER"))
+	fmt.Println("Application Initialised " + time.Now().UTC().String())
+	startServer()
+}
+
+func startServer() {
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+	})
+
+	// open a TCP port on 8080 and serve a handler to handle requests on incoming connections - start a HTTP 1.1 server.
+	http.ListenAndServe(":8080", nil)
 }
